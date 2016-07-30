078aba887270c9b0efcd6ae61b405eefecccd217
rstevens03 <rstevens03@gsb.columbia.edu>
2016-07-30 14:22:43 +0000

    will this commit to both

--- server.js
+++ server.js
@@ -14,3 +14,14 @@ route.for("/start", function(request, response){
     response.end();
 });
 
+function process_request(req, res) {
+    var body = 'Thanks for calling!\n';
+    var content_length = body.length;
+    
+    res.writeHead(200, {'Content-Length': content_length, 'Content-TYpe': 'text/plain'});
+    res.end(body);
+}
+
+var s = http.createServer(process_request);
+s.listen(8080);
+
